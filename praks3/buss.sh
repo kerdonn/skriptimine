bab1db8b97407652f0f3cf0716b8851e25aaba19
kerdonn <kerdon.laubert@voco.ee>
2026-09-04 11:12:19 +0300

    Ekskursiooni kalkulaator

--- praks3/buss.sh
+++ praks3/buss.sh
@@ -1,11 +1,11 @@
 echo -n "Sisesta reisijate arv: "
 read reisijad
 
-echo -n "Sisesta kohtade arv bussis: "
+echo -n "Sisesta kohtade arv ühes bussis: "
 read kohad
 
 bussid=`expr $reisijad / $kohad`
 maha=`expr $reisijad % $kohad`
 
 echo "Täielikult täidetud busse: $bussid"
-echo "Maha jäänud inimesi: $maha"
+echo "Maha jääb inimesi: $maha"
